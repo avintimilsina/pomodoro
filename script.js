33d2aa816cd1196c6491ab4c5b7b895f470bd1b9
Rohit Sah <sahrohit9586@gmail.com>
2022-07-14 19:58:01 +0545

    Added Flexboxs & Timer Stopping Logic

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//Develop tab functionality
-
-//1. Select all tabs from the list
+const body = document.querySelector("body");
 
 const pomodoroTab = document.getElementById("pomodoro");
 const shortBreakTab = document.getElementById("shortBreak");
@@ -10,101 +8,92 @@ const pomodoroContent = document.getElementById("pomodoro-content");
 const shortBreakContent = document.getElementById("shortbreak-content");
 const longBreakContent = document.getElementById("longbreak-content");
 
+const pomodoroTime = document.getElementById("pomodoroTime");
+const shortBreakTime = document.getElementById("shortBreakTime");
+const longBreakTime = document.getElementById("longBreakTime");
 
+const pomodoroButton = document.getElementById("pomodoroButton");
+const shortBreakButton = document.getElementById("shortBreakButton");
+const longBreakButton = document.getElementById("longBreakButton");
 
-//2. Add click event listener to each tab
-//3. Remove all active tab class from tab list
-//4. Add active class to selected tab
-//5. Remove all active class from tab content
-//6. Add active class to selected tab content
+const tabs = [shortBreakTab, longBreakTab, pomodoroTab];
+const contents = [pomodoroContent, shortBreakContent, longBreakContent];
 
-pomodoroTab.addEventListener("click", function () {
+function addActiveClass(tabElement, contentElement) {
+  tabElement.addEventListener("click", function () {
+    tabElement.classList.add("active");
+    tabs
+      .filter((tab) => tab !== tabElement)
+      .forEach((tab) => tab.classList.remove("active"));
+    contentElement.classList.add("active");
+    contents
+      .filter((content) => content !== contentElement)
+      .forEach((content) => content.classList.remove("active"));
 
+    if (tabElement === pomodoroTab) {
+      body.style.backgroundColor = "#e63946";
+      pomodoroButton.style.color = "#e63946";
+    } else if (tabElement === shortBreakTab) {
+      body.style.backgroundColor = "#4c9195";
+      shortBreakButton.style.color = "#4c9195";
+    } else if (tabElement === longBreakTab) {
+      body.style.backgroundColor = "#457ca3";
+      longBreakButton.style.color = "#457ca3";
+    }
+  });
+}
 
-  shortBreakTab.classList.remove("active");
-  longBreakTab.classList.remove("active");
-  pomodoroTab.classList.add("active");
+addActiveClass(pomodoroTab, pomodoroContent);
+addActiveClass(shortBreakTab, shortBreakContent);
+addActiveClass(longBreakTab, longBreakContent);
 
-  pomodoroContent.classList.add("active");
-  shortBreakContent.classList.remove("active");
-  longBreakContent.classList.remove("active");
+function convertToTwoDigits(num) {
+  return num.toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+}
 
-});
+let timerInterval;
 
-shortBreakTab.addEventListener("click", function () {
-  pomodoroTab.classList.remove("active");
-  longBreakTab.classList.remove("active");
-  shortBreakTab.classList.add("active");
-
-
-  pomodoroContent.classList.remove("active");
-  shortBreakContent.classList.add("active");
-  longBreakContent.classList.remove("active");
-
-});
-
-longBreakTab.addEventListener("click", function () {
-  pomodoroTab.classList.remove("active");
-  shortBreakTab.classList.remove("active");
-  longBreakTab.classList.add("active");
-
-
-  pomodoroContent.classList.remove("active");
-  shortBreakContent.classList.remove("active");
-  longBreakContent.classList.add("active");
-
-});
-
-// Timer to develop
-
-function timer(seconds) {
+function timer(seconds, element, button) {
   const now = Date.now();
-  const then = now + (seconds * 1000);
+  const then = now + seconds * 1000;
 
-  const timerInterval = setInterval(function () {
+  timerInterval = setInterval(function () {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-    displayTime(secondsLeft, pomodoroTime);
-    displayTime(secondsLeft,shortBreakTime);
-    displayTime(secondsLeft,longBreakTime);
+    displayTime(secondsLeft, element);
 
     if (secondsLeft < 1) {
       clearInterval(timerInterval);
+      element.innerHTML = `${convertToTwoDigits(seconds / 60)} : 00`;
+      button.innerText = "START";
     }
-
   }, 1000);
-
-};
+}
 
 function displayTime(seconds, element) {
   const minute = Math.floor(seconds / 60);
   const second = seconds % 60;
 
   console.log({ minute, second });
-
-  element.innerText = minute + " : " + second;
+  element.innerText =
+    convertToTwoDigits(minute) + " : " + convertToTwoDigits(second);
 }
-const pomodoroTime = document.getElementById("pomodoroTime");
-const shortBreakTime = document.getElementById("shortBreakTime");
-const longBreakTime = document.getElementBId("longBreakTime");
 
-const pomodoroButton = document.getElementById("pomodoroButton");
-const shortBreakButton = document.getElementById("shortBreakButton");
-const longBreakButton = document.getElementById("longBreakButton");
+function startStop(button, timeText, totalTime) {
+  button.addEventListener("click", function () {
+    if (button.innerText === "START") {
+      timer(totalTime, timeText, button);
+      button.innerText = "STOP";
+    } else {
+      clearInterval(timerInterval);
+      timeText.innerHTML = `${convertToTwoDigits(totalTime / 60)} : 00`;
+      button.innerText = "START";
+    }
+  });
+}
 
-pomodoroButton.addEventListener("click", function () {
-  const pomodoroTotalTime = 1500;
-  timer(pomodoroTotalTime);
-});
-
-shortBreakButton.addEventListener("click", function () {
-  const shortBreakTotalTime = 300;
-  timer(shortBreakTotalTime);
-});
-longBreakButton.addEventListener("click", function () {
-  const longBreakTotalTime = 900;
-  timer(longBreakTotalTime);
-});
-
-
-
-
+startStop(pomodoroButton, pomodoroTime, 1500);
+startStop(shortBreakButton, shortBreakTime, 300);
+startStop(longBreakButton, longBreakTime, 900);
